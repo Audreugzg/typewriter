cb9525b7acc5c16169b01edda0d108f62e871cc7
Zuge Gao <audreygaozu@hotmail.com>
2022-06-07 17:29:43 +0000

    animated sentence

---
+++ async/hello_timeout.js
@@ -0,0 +1,6 @@
+//Challenge #1
+setTimeout(() => console.log('4311o th3r3 w0r1d'),3000);
+//Challenge #2
+setTimeout(function(){console.log('4311o');},1000);
+setTimeout(function(){console.log('th3r3');},2000);
+setTimeout(function(){console.log('w0r1d');},3000);
---
+++ javascripting/function-arguments.js
@@ -0,0 +1,6 @@
+function math(x, y, z){
+  var rel = x + (y * z);
+  return rel;
+}
+
+console.log(math(53, 61, 67));
