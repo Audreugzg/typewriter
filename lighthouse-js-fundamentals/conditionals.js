697ae719f1353883549bda734d19a8a00cbd885e
Zuge Gao <audreygaozu@hotmail.com>
2022-06-07 18:42:36 +0000

    merge

--- lighthouse-js-fundamentals/conditionals.js
+++ lighthouse-js-fundamentals/conditionals.js
@@ -1,3 +1,16 @@
+const raining = true;
+const cold = false;
+
+if (raining) {
+  console.log("Don't forget your umbrella!");
+}
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+}
+
+console.log("Now you're ready to go outside!");
+
 const cold = false;
 
 if (cold) {
@@ -5,6 +18,7 @@ if (cold) {
 } else {
   console.log("Short sleeves are fine.");
 }
+
 const temperature = 12;
 
 if (temperature < 0) {
